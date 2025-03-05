edb6a84a5315753c8824164f091d6eaaf2ffee4d
Daniel Carvalho <ermeterio@gmail.com>
2025-03-05 10:39:20 -0300

    Develop (#9)
    * Feature/domain implementation (#1)
    
    * feat: development of initial modeling of relationships between domain entities
    
    * feat: desenvolvimento da abstração para inserção no banco e da camada do mongo.
    
    * feat: develop request structure
    
    * feat: develop application handlers and tests
    
    * feat: develop controllers, initial tests and rabbitmq and rebus support.
    
    * feat: develop initial handlers
    
    * feat: implements more tests and adjust features.
    
    * feat: final adjusts in domain and application handlers
    
    * feat: finishing the code but with incomplete unit tests, this test is very extensive and cannot fully extract the developer's knowledge, I recommend reducing the complexity and making the code less verbose.
    
    ---------
    
    Co-authored-by: Daniel Carvalho <daniel.ermeterio@hotmail.com>
    
    * Update readme (#6)
    
    * Test
    
    * test
    
    * Feature/domain implementation (#1) (#2)
    
    * feat: development of initial modeling of relationships between domain entities
    
    * feat: desenvolvimento da abstração para inserção no banco e da camada do mongo.
    
    * feat: develop request structure
    
    * feat: develop application handlers and tests
    
    * feat: develop controllers, initial tests and rabbitmq and rebus support.
    
    * feat: develop initial handlers
    
    * feat: implements more tests and adjust features.
    
    * feat: final adjusts in domain and application handlers
    
    * feat: finishing the code but with incomplete unit tests, this test is very extensive and cannot fully extract the developer's knowledge, I recommend reducing the complexity and making the code less verbose.
    
    ---------
    
    Co-authored-by: Daniel Carvalho <daniel.ermeterio@hotmail.com>
    
    * Feature/domain implementation (#1) (#3) (#4)
    
    * feat: development of initial modeling of relationships between domain entities
    
    * feat: desenvolvimento da abstração para inserção no banco e da camada do mongo.
    
    * feat: develop request structure
    
    * feat: develop application handlers and tests
    
    * feat: develop controllers, initial tests and rabbitmq and rebus support.
    
    * feat: develop initial handlers
    
    * feat: implements more tests and adjust features.
    
    * feat: final adjusts in domain and application handlers
    
    * feat: finishing the code but with incomplete unit tests, this test is very extensive and cannot fully extract the developer's knowledge, I recommend reducing the complexity and making the code less verbose.
    
    ---------
    
    Co-authored-by: Daniel Carvalho <daniel.ermeterio@hotmail.com>
    
    * Update README.md (#5)
    
    ---------
    
    Co-authored-by: Daniel Carvalho <daniel.ermeterio@hotmail.com>
    
    * feat: implement more tests (#8)
    
    Co-authored-by: Daniel Carvalho <daniel.ermeterio@hotmail.com>
    
    ---------
    
    Co-authored-by: Daniel Carvalho <daniel.ermeterio@hotmail.com>

--- src/Ambev.DeveloperEvaluation.Domain/Repositories/ICategoryRepository.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ICategoryRepository.cs
@@ -4,5 +4,6 @@ namespace Ambev.DeveloperEvaluation.Domain.Repositories
 {
     public interface ICategoryRepository : IRepository<Category>
     {
+        public Task<Category?> GetByNameAsync(string name, Guid companyId);
     }
 }
--- tests/Ambev.DeveloperEvaluation.Unit/Application/Sale/CreateSaleHandlerTests.cs
+++ tests/Ambev.DeveloperEvaluation.Unit/Application/Sale/CreateSaleHandlerTests.cs
@@ -1,12 +1,9 @@
 ﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
-using Ambev.DeveloperEvaluation.Domain.Entities.Sale;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
-using Ambev.DeveloperEvaluation.Unit.Application.Product.Fixture;
 using Ambev.DeveloperEvaluation.Unit.Application.Sale.Fixture;
 using AutoMapper;
 using NSubstitute;
 using NSubstitute.ReturnsExtensions;
-using Rebus.Compression;
 using Xunit;
 
 namespace Ambev.DeveloperEvaluation.Unit.Application.Sale
@@ -32,7 +29,7 @@ namespace Ambev.DeveloperEvaluation.Unit.Application.Sale
 
             var mapper = Substitute.For<IMapper>();
             mapper.Map<DeveloperEvaluation.Domain.Entities.Sale.Sale>(Arg.Any<CreateSaleCommand>()).Returns(_fixture.GetValidSale());
-            
+
             var companyRepository = Substitute.For<ICompanyRepository>();
             companyRepository.GetByIdAsync(Arg.Any<Guid>()).Returns(new DeveloperEvaluation.Domain.Entities.Company.Company("test", null, "1234567"));
 
@@ -49,6 +46,27 @@ namespace Ambev.DeveloperEvaluation.Unit.Application.Sale
         }
         #endregion
         #region Error
+        [Fact(DisplayName = nameof(Should_Be_Create_Sale_With_Not_Found_Company))]
+        [Trait("Sale", nameof(CreateSaleHandlerTests))]
+        public async Task Should_Be_Create_Sale_With_Not_Found_Company()
+        {
+            //arrange
+            var sale = _fixture.GetValidSale();
+            var saleRepository = Substitute.For<ISaleRepository>();
+            saleRepository.GetExistingSaleForUser(Arg.Any<Guid>(), CancellationToken.None).ReturnsNull();
+
+            var companyRepository = Substitute.For<ICompanyRepository>();
+            companyRepository.GetByIdAsync(Guid.NewGuid()).ReturnsNull();
+
+            var commandHandler = new CreateSaleHandler(saleRepository, Substitute.For<IMapper>(), Substitute.For<IProductRepository>(), companyRepository);
+
+            //act
+            var exceptions = await Record.ExceptionAsync(() => commandHandler.Handle(_fixture.ValidCreateSaleCommandMock(), CancellationToken.None));
+
+            //assert
+            Assert.NotNull(exceptions);
+        }
+
         [Fact(DisplayName = nameof(Should_Be_Create_Sale_With_Existing_Open_Sale))]
         [Trait("Sale", nameof(CreateSaleHandlerTests))]
         public async Task Should_Be_Create_Sale_With_Existing_Open_Sale()
@@ -70,9 +88,9 @@ namespace Ambev.DeveloperEvaluation.Unit.Application.Sale
             Assert.NotNull(exceptions);
         }
 
-        [Fact(DisplayName = nameof(Should_Be_Create_Sale_With_Invalid_Sale))]
+        [Fact(DisplayName = nameof(Should_Be_Create_Sale_With_Not_Found_Product))]
         [Trait("Sale", nameof(CreateSaleHandlerTests))]
-        public async Task Should_Be_Create_Sale_With_Invalid_Sale()
+        public async Task Should_Be_Create_Sale_With_Not_Found_Product()
         {
             //arrange
             var sale = _fixture.GetInvalidSale();
@@ -82,7 +100,34 @@ namespace Ambev.DeveloperEvaluation.Unit.Application.Sale
             var companyRepository = Substitute.For<ICompanyRepository>();
             companyRepository.GetByIdAsync(Guid.NewGuid()).Returns(new DeveloperEvaluation.Domain.Entities.Company.Company("test", null, "1234567"));
 
-            var commandHandler = new CreateSaleHandler(saleRepository, Substitute.For<IMapper>(), Substitute.For<IProductRepository>(), companyRepository);
+            var productRepository = Substitute.For<IProductRepository>();
+            productRepository.GetByIdAsync(Arg.Any<Guid>()).ReturnsNull();
+
+            var commandHandler = new CreateSaleHandler(saleRepository, Substitute.For<IMapper>(), productRepository, companyRepository);
+
+            //act
+            var exceptions = await Record.ExceptionAsync(() => commandHandler.Handle(_fixture.ValidCreateSaleCommandMock(), CancellationToken.None));
+
+            //assert
+            Assert.NotNull(exceptions);
+        }
+
+        [Fact(DisplayName = nameof(Should_Be_Create_Sale_With_Empty_Items))]
+        [Trait("Sale", nameof(CreateSaleHandlerTests))]
+        public async Task Should_Be_Create_Sale_With_Empty_Items()
+        {
+            //arrange
+            var sale = _fixture.GetInvalidSaleWithoutItems();
+            var saleRepository = Substitute.For<ISaleRepository>();
+            saleRepository.GetExistingSaleForUser(Arg.Any<Guid>(), CancellationToken.None).Returns(sale);
+
+            var companyRepository = Substitute.For<ICompanyRepository>();
+            companyRepository.GetByIdAsync(Guid.NewGuid()).Returns(new DeveloperEvaluation.Domain.Entities.Company.Company("test", null, "1234567"));
+
+            var productRepository = Substitute.For<IProductRepository>();
+            productRepository.GetByIdAsync(Arg.Any<Guid>()).ReturnsNull();
+
+            var commandHandler = new CreateSaleHandler(saleRepository, Substitute.For<IMapper>(), productRepository, companyRepository);
 
             //act
             var exceptions = await Record.ExceptionAsync(() => commandHandler.Handle(_fixture.ValidCreateSaleCommandMock(), CancellationToken.None));
